ff4fdcbd2703f103679a49a975549e41203f3a1d
Christian Andrés Díaz Cuadro <108769701+CDiazCuadro@users.noreply.github.com>
2022-09-19 22:31:43 -0300

    Updating Ejercicio4
    Signed-off-by: Christian Andrés Díaz Cuadro <108769701+CDiazCuadro@users.noreply.github.com>

--- Obl2/Ejercicio4.m
+++ Obl2/Ejercicio4.m
@@ -33,18 +33,18 @@ H(n+1:2*n, 2*n+1:3*n) = -eye(n);
 delta = zeros(3*n,1);
 delta = [alpha; beta; gamma];  
 
-## Calculo solucion exacta x_op = 2m\(M+M')
+## Calculo solucion exacta x_op = inv(M)*m
 # Defino M, m y mu
 M  = A'*A + B'*B + C'*C;
 m  = A'*alpha + B'*beta + C'*gamma;
 mu = alpha'*alpha + beta'*beta + gamma'*gamma;
-x_op = (2*m\(M+M'))';
+x_op = inv(M)*m
 
 ## Calculo loa multiplicadores lambda_op
-lambda_op = (H*D'*delta\(H*H'))';
+lambda_op = inv(H*H')*H*D'*delta;
 
 ## Solucion Nummerica:
-e_max = 1e-8;
+diff_max = 1e-8;
 k_max = 100 ;
 t0 = 1/norm(D);
 w0 = rand(30,1);
@@ -53,13 +53,22 @@ w0 = rand(30,1);
 # Parte e) lambda = lambda_op
 #          tk = t02^k.
 
-lambda = lambda_op;
-k      = 0        ;  
-e_act  = e_max + 1;
+lambda = lambda_op ;
+k      = 0          ;  
+diff  = diff_max + 1;
+w_hist  = [w0]      ; 
+diff_hist = [diff]  ;
 
-while e_act > e_max && k < k_max
+while diff > diff_max && k < k_max
       k = k + 1 ;
       t = t0*2^k;
-      ...
+      w_old = w_hist(:,end);
+      w = inv( (1/2)*(D'*D + D*D') + t*H'*H)*(D'*delta - H'*lambda) ;
+      diff = norm(w - w_old)/norm(w);
+      diff_hist = [diff_hist,diff]  ;
+      w_hist = [w_hist,w];
+      
+      
   
 end
+x_num = (w(1:10) + w(11:20) + w(21:30))/3
